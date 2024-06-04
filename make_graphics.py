5a6f2f4b439429255a097f65fb2206636693ff44
Zhuoming Chen <zhuominc@lovelace.ece.local.cmu.edu>
2024-06-04 11:23:11 -0400

    es

--- make_graphics.py
+++ make_graphics.py
@@ -3,11 +3,16 @@ import json
 import numpy as np
 from itertools import product
 
+baseline = {
+    "meta-llama/Meta-Llama-3-8B-Instruct":0.76,
+    "meta-llama/Llama-2-7b-chat-hf":0.16,
+    "meta-llama/Llama-2-13b-chat-hf":0.34
 
+}
 model = ["meta-llama/Meta-Llama-3-8B-Instruct", "meta-llama/Llama-2-7b-chat-hf", "meta-llama/Llama-2-13b-chat-hf"]
 sparse = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 random_sparse = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-method = ["", ",oracle=True", ",random=True"]
+method = ["", ",oracle=True", ",random=True", ",anns_es=True"]
 for m_s in product(model, sparse):
     MODEL = m_s[0]
     SPARSE = m_s[1]
@@ -15,14 +20,17 @@ for m_s in product(model, sparse):
     VALID_NUMBER = 0
 
     
-
+    
     for i, pack in enumerate(product(model, sparse, random_sparse, method)):
+        
         if pack[1] + pack[2] > 0.95 or pack[1] != SPARSE or pack[0] != MODEL: continue
         name = pack[0].split("/")[1] + "-" + str(pack[1]) + "-" + str(pack[2])
         if pack[3] == ",oracle=True":
             name = name + "-oracle"
         elif pack[3] == ",random=True":
             name = name + "-random"
+        elif pack[3] == ",anns_es=True":
+            name = name + "-anns_es-ANNSES"
         else:
             name = name + "-ann-ALSH-X"
         log_name = "results/" + name + ".log"+"/results.json"
@@ -36,23 +44,29 @@ for m_s in product(model, sparse):
             VALID_NUMBER += 1
         elif pack[3] == ",random=True":
             RESULTS[2][int(10 * pack[2])] = em
+        elif pack[3] == ",anns_es=True":
+            RESULTS[3][int(10 * pack[2])] = em
         else:
             RESULTS[0][int(10 * pack[2])] = em
         
         
 
     
+    
     if VALID_NUMBER > 0:
         import matplotlib.pyplot as plt
 
         plt.figure(figsize=(20, 10), dpi=100)
+        plt.axhline(baseline[MODEL])
         plt.plot(random_sparse[:VALID_NUMBER], RESULTS[0][:VALID_NUMBER], c="blue", label="anns")
         plt.plot(random_sparse[:VALID_NUMBER], RESULTS[1][:VALID_NUMBER], c="green", label="oracle")
         plt.plot(random_sparse[:VALID_NUMBER], RESULTS[2][:VALID_NUMBER], c="red", label="random")
+        plt.plot(random_sparse[:VALID_NUMBER], RESULTS[3][:VALID_NUMBER], c="orange", label="anns-es")
         plt.legend()
         plt.title(MODEL + "-" + str(SPARSE))
         plt.savefig("pictures/"+ MODEL.split("/")[1] + "-" + str(SPARSE)+".pdf")
         plt.clf()
+    
             
 
             
