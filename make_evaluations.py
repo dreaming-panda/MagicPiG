39683614ee459cd9e79e477beb13fea7eff65420
dreaming-panda <chenzhuoming911@gmail.com>
2024-06-05 17:24:43 +0000

    pdfs

--- make_evaluations.py
+++ make_evaluations.py
@@ -1,5 +1,5 @@
 model = ["meta-llama/Llama-2-7b-chat-hf", "meta-llama/Meta-Llama-3-8B", "meta-llama/Llama-2-13b-chat-hf", "meta-llama/Meta-Llama-3-8B-Instruct"]
-TASK = "ANNSES1"
+TASK = "ANNSES2"
 sparse = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 random_sparse = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 method = [",anns_es1=True"]
@@ -20,7 +20,7 @@ for i, pack in enumerate(product(model, sparse, random_sparse, method)):
     log_name = name + "-{}.log".format(TASK)
     bash_name = "bashes/" + name + "-mgp{}.sh".format(TASK)
     script_name = "scripts/" + name + "-mgp{}.sh".format(TASK)
-    cmd = "accelerate launch main.py --model xhf --tasks gsm8k_cot  --batch_size 1 --model_args pretrained={},search=True,sparse={},random_sparse={},vsparse=1.0,window_size=64,K=4,L=25{} --output_path results/{}".format(pack[0], pack[1], pack[2], pack[3], log_name)
+    cmd = "accelerate launch main.py --model xhf --tasks gsm8k_cot  --batch_size 1 --model_args pretrained={},search=True,sparse={},random_sparse={},vsparse=1.0,window_size=16,K=4,L=25{} --output_path results/{}".format(pack[0], pack[1], pack[2], pack[3], log_name)
     
     script_cmd = """#!/bin/bash
 ## job name
